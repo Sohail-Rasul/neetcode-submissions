2fa7a53652225c41aecad1c6e4c7068f761fbf3e
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-06-14 17:28:28 +0530

    Add: find-the-difference-of-two-arrays - submission-3

---
+++ Data Structures & Algorithms/find-the-difference-of-two-arrays/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> ans;
+        vector<int> dist1;
+        vector<int> dist2;
+
+        for(int i =0;i<nums1.size() ;i++){
+            if(find(dist1.begin(), dist1.end() , nums1[i]) == dist1.end()){
+                dist1.push_back(nums1[i]);
+            }
+        }
+
+
+        int ctr=0;
+        for(int i =0;i<nums2.size();i++){
+            if(find(dist2.begin(), dist2.end() , nums2[i]) == dist2.end()){
+                dist2.push_back(nums2[i]);
+            }
+
+            if(find(dist1.begin(), dist1.end() , nums2[i]) != dist1.end()){
+                auto it = find(dist1.begin(), dist1.end(), nums2[i]);
+                dist1.erase(it);
+            }
+        }
+
+        for(int i =0;i<nums1.size();i++){
+            if(find(dist2.begin(), dist2.end() , nums1[i]) != dist2.end()){
+                auto it = find(dist2.begin(), dist2.end(), nums1[i]);
+                dist2.erase(it);
+            }
+        }
+
+        ans.push_back(dist1);
+        ans.push_back(dist2);
+
+        return ans;
+    }
+};
