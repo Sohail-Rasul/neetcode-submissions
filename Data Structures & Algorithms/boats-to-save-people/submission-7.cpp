2043b966f9e9fbbdbbad3706d8f46f17bd1e321b
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-06-21 20:35:32 +0530

    Add: boats-to-save-people - submission-7

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int l = 0;
+        int r = people.size()-1;
+        int count =0;
+        while(l<=r){
+            if(people[l]+people[r]>limit){
+                count++;
+                r--;
+            }
+            else if(people[l]+people[r]<=limit){
+                count++;
+                l++;
+                r--;
+            }
+        }
+        return count;
+    }
+};
