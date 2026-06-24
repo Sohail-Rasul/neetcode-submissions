a3af87de49106064400eacfb1211c8d97a1b7a56
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-06-24 22:32:32 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l =0;
+        int r = prices.size()-1;
+        int max_profit=0;
+        while(l<r){
+            int profit = prices[r]-prices[l];
+            cout << "Bought At: " << prices[l] << "\n";
+            cout << "Sold At: "  << prices[r] << "\n";
+            cout << "Profit: " << profit << "\n";
+            if(profit>max_profit){
+                max_profit = profit;
+            }
+            if(r==l+1){
+                r=prices.size()-1;
+                l++;
+                continue;
+            }
+            else{
+                r--;
+                continue;
+            }
+            
+        }
+
+        return max_profit;
+    }
+};
