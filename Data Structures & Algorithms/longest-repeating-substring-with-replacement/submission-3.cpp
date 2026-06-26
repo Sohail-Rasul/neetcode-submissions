9d707677d57711f9799a8e338d992f4276674c12
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-06-26 16:22:04 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res =0;
+        int l=0, maxFreq = 0;
+
+        unordered_map<char,int> count;
+
+        for(int r = 0;r<s.size();r++){
+            count[s[r]]++;
+            maxFreq = max(maxFreq,count[s[r]]);
+
+            while( (r-l+1) - maxFreq >k ){
+                count[s[l]]--;
+                l++;
+            }
+
+            res = max(res,r-l+1);
+        }
+
+        return res;
+    }
+};
