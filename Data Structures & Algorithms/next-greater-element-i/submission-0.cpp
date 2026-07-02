23fad3094076372a91ae721072152337927d6828
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-07-02 13:49:12 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> res(nums1.size(),-1);
+        unordered_map<int,int> idx;
+        
+        for(int i =0;i<nums1.size();i++){
+            idx[nums1[i]] =i;
+        }
+
+        for(int i =0;i<nums2.size();i++){
+            if(st.empty() && idx.find(nums2[i]) != idx.end()){
+                st.push(nums2[i]);
+                if(i==nums2.size()-1){
+                    res[idx[st.top()]] = -1;
+                }
+                continue;
+            }
+            else if(st.empty()) continue;
+
+            if(nums2[i] <= st.top()){
+                if(idx.find(nums2[i]) != idx.end()){
+                    st.push(nums2[i]);
+                    continue;
+                }
+            }
+            else{
+                while(!st.empty() && nums2[i] > st.top()){
+                    if(idx.find(nums2[i]) != idx.end()){
+                        res[idx[st.top()]] = nums2[i];
+                        st.pop();
+                        st.push(nums2[i]);
+                    }
+                    else{
+                        res[idx[st.top()]] = nums2[i];
+                        st.pop();
+                    }
+                }
+                
+            }
+            
+
+
+        }
+
+        return res;
+    }
+};
