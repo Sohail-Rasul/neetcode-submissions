943693f4f521cd111ad20c101f61ccceb7e46521
Mohammed Sohail Rasul <69708396+Sohail-Rasul@users.noreply.github.com>
2026-08-01 19:11:50 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26, 0);
+        // Count of each task
+        for (char task : tasks) {
+            count[task - 'A']++;
+        }
+
+        // Greedy: Perform most frequent task
+        priority_queue<int> maxHeap;
+        for (int cnt : count) {
+            if (cnt > 0) {
+                maxHeap.push(cnt);
+            }
+        }
+
+        // Track Tasks in cooldown before they can be executed again
+        int time = 0;
+        queue<pair<int, int>> q;
+
+        while (!maxHeap.empty() || !q.empty()) {
+            time++;
+
+            if (maxHeap.empty()) {
+                time = q.front().second;
+            } else {
+                int cnt = maxHeap.top() - 1;
+                maxHeap.pop();
+                if (cnt > 0) {
+                    q.push({cnt, time + n});
+                }
+            }
+
+            if (!q.empty() && q.front().second == time) {
+                maxHeap.push(q.front().first);
+                q.pop();
+            }
+        }
+
+        return time;
+    }
+};
